464102757a10ea558ee0b37c3e718301fa6bf6f2
Sid <sid.ss8805@gmail.com>
2024-01-28 15:38:30 +0530

    demo

--- server.ts
+++ server.ts
@@ -2,6 +2,9 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
+import { createuser } from "src/controllers/createuser";
+import { getuser } from "src/controllers/getusers";
+import { getusermiddleware } from "src/middlewares/getuser";
 
 const app = express();
 const PORT = 5000;
@@ -10,6 +13,7 @@ app
     .use(cors())
     .use(helmet())
     .use(morgan("dev"))
+    .use(express.json())
 
 app.get("/", (req: Request, res: Response) => {
     return res
@@ -18,6 +22,10 @@ app.get("/", (req: Request, res: Response) => {
             "msg": "Success"
         })
 })
+
+app.post("/createuser", createuser)
+
+app.post("/getuser", getusermiddleware, getuser)
 
 app.listen(PORT, () => {
     console.log(`Service active on PORT ${PORT}`)
