004301bb76086919914760a744b4e6b5cc2e2245
ruler45 <sahin0945@gmail.com>
2024-02-08 19:26:24 +0530

    some changes done

--- server.ts
+++ server.ts
@@ -3,8 +3,11 @@ import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
 import 'dotenv/config';
+import bodyParser from "body-parser";
 import { nanoid } from 'nanoid';
+import { PrismaClient } from "@prisma/client";
 
+const prisma = new PrismaClient();
 
 
 const app = express();
@@ -14,6 +17,8 @@ app
     .use(cors())
     .use(helmet())
     .use(morgan("dev"))
+    .use(bodyParser.json());
+    
 
 app.get("/", (req: Request, res: Response) => {
     return res
@@ -27,12 +32,18 @@ app.listen(PORT, () => {
     console.log(`Service active on PORT ${PORT}`)
 })
 
-app.post("/api/shorten",( req:Request,res:Response)=>{
 
-    // const shortid=nanoid(6);
-    // console.log(shortid);
-    const original =req.body;
-    console.log(original);
+
+
+
+app.post("/api/shorten",async ( req:Request,res:Response)=>{
+
+    const url= await prisma.user.findUnique({})
+
+    const shortid=nanoid(6);
+    console.log(shortid);
+    const originalUrl =req.body.originalUrl;
+    console.log(originalUrl);
     return res.status(200).json({
         "msg":"API Success"
     })
