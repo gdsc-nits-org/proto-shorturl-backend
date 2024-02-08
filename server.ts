2fd167ea2dac8cd5d62e5ab4f2777d8cb90c37ed
ruler45 <sahin0945@gmail.com>
2024-02-08 19:08:25 +0530

    nanoid added

--- server.ts
+++ server.ts
@@ -2,9 +2,13 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
+import 'dotenv/config';
+import { nanoid } from 'nanoid';
+
+
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 3000;
 
 app
     .use(cors())
@@ -21,4 +25,15 @@ app.get("/", (req: Request, res: Response) => {
 
 app.listen(PORT, () => {
     console.log(`Service active on PORT ${PORT}`)
+})
+
+app.post("/api/shorten",( req:Request,res:Response)=>{
+
+    // const shortid=nanoid(6);
+    // console.log(shortid);
+    const original =req.body;
+    console.log(original);
+    return res.status(200).json({
+        "msg":"API Success"
+    })
 })
