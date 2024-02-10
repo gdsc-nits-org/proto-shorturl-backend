80402d4364e8732e37ca44b70f15b8dd215ded75
Niraj Kumar <nk931685@gmail.com>
2024-02-10 23:36:37 +0530

    Login Signup Auth

--- server.ts
+++ server.ts
@@ -20,15 +20,15 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors()).use(helmet()).use(morgan("dev")).use(express.json());
 
-app.get("/",async (req: Request, res: Response) => {
-  
+app.get("/", async (req: Request, res: Response) => {
 
-  const data=await prisma.url.findMany({});
-  
+
+  const data = await prisma.url.findMany({});
+
   return res.status(201).json({
     msg: "Success",
     request: req.body,
-    data:data
+    data: data
   });
 });
 
@@ -50,17 +50,18 @@ app.post("/api/shorten", async (req: Request, res: Response) => {
 
   const existingUrl = await prisma.url.findMany({
     where: {
-      longUrl: longUrl
+      longUrl: longUrl,
     },
   });
 
-  console.log(existingUrl)
-  if(existingUrl){
+
+  if (existingUrl) {
+    console.log(existingUrl);
     return res.status(403).json({
       originalUrl: existingUrl[0].longUrl,
       shortUrl: existingUrl[0].shortUrl,
       data: existingUrl
-      
+
     })
   }
 
@@ -71,11 +72,14 @@ app.post("/api/shorten", async (req: Request, res: Response) => {
         shortUrl: url.shortUrl,
       },
     });
+
     if (result) {
+
       return res.status(201).json({
         message: "ShortUrl successfully created",
         originalUrl: result.longUrl,
         shortUrl: result.shortUrl,
+
       });
     } else {
       return res.status(501).json({ error: "Couldn't create shortUrl" });
@@ -95,13 +99,13 @@ app.get("/api/shortUrl/:shortUrl", async (req: Request, res: Response) => {
     },
   });
   if (url) {
-    const updatedUrl=await prisma.url.update({
-      where:{
+    const updatedUrl = await prisma.url.update({
+      where: {
         shortUrl: shortUrl
       },
-      data:{
-        clickCount: url.clickCount +1,
-      
+      data: {
+        clickCount: url.clickCount + 1,
+        // updatedtAt : new Date(),
       }
     })
     return res.redirect(updatedUrl.longUrl);
@@ -118,93 +122,93 @@ app.get("/api/shortUrl/:shortUrl", async (req: Request, res: Response) => {
 
 
 // Sign-up route
-app.post('/signup', async (req: Request, res: Response) => {
+app.post('/signup', validateInput, async (req: Request, res: Response) => {
   const { username, email, password } = req.body;
 
   try {
     // Check if the email already exists
-  const existingUser = await prisma.user.findUnique({ where: { email } });
-  if (existingUser) {
-    // Email already exists, redirect to the login page
-    
-    return res.redirect('/login');
-    
-  }
+    const existingUser = await prisma.user.findUnique({ where: { email } });
+    if (existingUser) {
+      // Email already exists, redirect to the login page
 
-  // Hash the password before storing it
-  const hashedPassword = await bcrypt.hash(password, 10);
+      return res.redirect('/login');
 
-  // Create a new user
-  const newUser = await prisma.user.create({
-    data: {
-      username,
-      email,
-      password: hashedPassword,
-    },
-  });
+    }
 
-   // Generate JWT token
-   const token = jwt.sign({ userId: newUser.id }, 'your-secret-key', { expiresIn: '1h' });
+    // Hash the password before storing it
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-   // Set cookie with the token
-   res.cookie('jwt', token, { httpOnly: true, maxAge: 3600000 }); // maxAge is in milliseconds (1 hour in this case)
+    // Create a new user
+    const newUser = await prisma.user.create({
+      data: {
+        username,
+        email,
+        password: hashedPassword,
+      },
+    });
 
-   // console.log(newUser);
-   return res.json({ message: 'User signed up successfully', user: newUser, token });
+    // Generate JWT token
+    const token = jwt.sign({ userId: newUser.id }, 'your-secret-key', { expiresIn: '1h' });
+
+    // Set cookie with the token
+    res.cookie('jwt', token, { httpOnly: true, maxAge: 3600000 }); // maxAge is in milliseconds (1 hour in this case)
+
+    // console.log(newUser);
+    return res.json({ message: 'User signed up successfully', user: newUser, token });
   } catch (error) {
     console.log(error);
-    return res.json({error})
+    return res.json({ error })
 
   }
 
-  
+
 });
 
 
-app.post('/login', async (req: Request, res: Response) => {
+app.post('/login', validateInput, async (req: Request, res: Response) => {
   const { username, email, password } = req.body;
 
-   try{
-      
-  // Check if the user is already signed up
-  const existingUser = await prisma.user.findFirst({
-    where: {
-      OR: [
-        { username: username },
-        { email: email },
-      ],
-    },
-  });
+  try {
 
-  if (!existingUser) {
-    // User not found, display an error message
-    return res.status(401).json({ message: 'Incorrect username or password' });
+    // Check if the user is already signed up
+    const existingUser = await prisma.user.findFirst({
+      where: {
+        OR: [
+          { username: username },
+          { email: email },
+        ],
+      },
+    });
+
+    if (!existingUser) {
+      // User not found, display an error message
+      return res.status(401).json({ message: 'Incorrect username or password' });
+    }
+
+    // Check if the password is correct
+    const passwordMatch = await bcrypt.compare(password, existingUser.password);
+    if (!passwordMatch) {
+      // Incorrect password, display an error message
+      return res.status(401).json({ message: 'Incorrect username or password' });
+    }
+
+
+    // Generate JWT token
+    const token = jwt.sign({ userId: existingUser.id }, 'your-secret-key', { expiresIn: '1h' });
+
+    // Set cookie with the token
+    res.cookie('jwt', token, { httpOnly: true, maxAge: 3600000 }); // maxAge is in milliseconds (1 hour in this case)
+
+    // Redirect to the /api/shorten route on successful login
+    // Replace with your desired URL
+    // res.redirect('/api/shorten'); // Replace with your desired URL
+
+    return res.json({ message: 'Login successful', user: existingUser, token });
   }
-
-  // Check if the password is correct
-  const passwordMatch = await bcrypt.compare(password, existingUser.password);
-  if (!passwordMatch) {
-    // Incorrect password, display an error message
-    return res.status(401).json({ message: 'Incorrect username or password' });
-  }
-
-  
-   // Generate JWT token
-   const token = jwt.sign({ userId: existingUser.id }, 'your-secret-key', { expiresIn: '1h' });
-
-   // Set cookie with the token
-   res.cookie('jwt', token, { httpOnly: true, maxAge: 3600000 }); // maxAge is in milliseconds (1 hour in this case)
-
-   // Redirect to the /api/shorten route on successful login
-   // Replace with your desired URL
-  // res.redirect('/api/shorten'); // Replace with your desired URL
-
-   return res.json({ message: 'Login successful', user: existingUser, token });
-   }
-   catch(error){
+  catch (error) {
     console.error(error);
-     res.status(500).json({ error: 'Internal Server Error' });
-   }
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 
 });
 
